b8f96196ac2da74cacf9f01ced4734f77bbebaab
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-05-11 18:54:52 +0200

    cs

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,29 +2,7 @@
 
 $rules = [
     '@Symfony' => true,
-    'blank_line_after_namespace' => true,
-    'blank_line_after_opening_tag' => true,
-    'constant_case' => [
-        'case' => 'lower'
-    ],
-    'declare_equal_normalize' => true,
-    'elseif' => true,
-    'encoding' => true,
-    'full_opening_tag' => true,
-    'function_declaration' => true,
-    'heredoc_to_nowdoc' => true,
-    'indentation_type' => true,
-    'line_ending' => true,
-    'lowercase_cast' => true,
-    'lowercase_keywords' => true,
-    'lowercase_static_reference' => true,
-    'multiline_whitespace_before_semicolons' => [
-        'strategy' => 'no_multi_line'
-    ],
-    'no_alias_functions' => true,
-    'no_blank_lines_after_class_opening' => true,
-    'no_closing_tag' => true,
-    'no_empty_comment' => false, // Symfony override
+    'no_empty_comment' => false,
     'no_extra_blank_lines' => [
         'tokens' => [
             'extra',
@@ -32,28 +10,9 @@ $rules = [
             'use',
             'use_trait',
         ]
-    ], // Symfony override
-    'no_leading_import_slash' => true,
-    'no_spaces_after_function_name' => true,
-    'no_spaces_inside_parenthesis' => true,
-    'no_trailing_whitespace' => true,
-    'no_trailing_whitespace_in_comment' => true,
-    'no_unreachable_default_argument_value' => true,
-    'no_useless_return' => true,
-    'no_whitespace_in_blank_line' => true,
+    ],
     'not_operator_with_successor_space' => true,
-    'php_unit_method_casing' => false, // Symfony override
-    'self_accessor' => true,
-    'short_scalar_cast' => true,
-    'simplified_null_return' => false,
-    'single_blank_line_at_eof' => true,
-    'single_blank_line_before_namespace' => true,
-    'single_import_per_statement' => true,
-    'single_line_after_imports' => true,
-    'space_after_semicolon' => true,
-    'switch_case_semicolon_to_colon' => true,
-    'switch_case_space' => true,
-    'ternary_operator_spaces' => true,
+    'php_unit_method_casing' => false,
 ];
 
 $finder = PhpCsFixer\Finder::create()
