7412e53c7c43f35d38b5eee9f49a36367f777fc3
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-05-15 18:23:21 +0200

    register

--- resources/admin/pages/auth/Register.vue
+++ resources/admin/pages/auth/Register.vue
@@ -1,1 +1,90 @@
-<template><h1>Register !</h1></template>
+<template>
+  <div>
+    <teleport to="head">
+      <title>{{ $title('Register') }}</title>
+    </teleport>
+    <auth-card>
+      <validation-errors class="mb-4" />
+
+      <form @submit.prevent="submit">
+        <div>
+          <base-input
+            v-model="form.name"
+            :label="$t('Name')"
+            type="text"
+            required
+            autofocus
+            autocomplete="name"
+          />
+        </div>
+
+        <div class="mt-4">
+          <base-input
+            v-model="form.email"
+            :label="$t('Email')"
+            type="email"
+            required
+          />
+        </div>
+
+        <div class="mt-4">
+          <base-input
+            v-model="form.password"
+            :label="$t('Password')"
+            type="password"
+            required
+            autocomplete="new-password"
+          />
+        </div>
+
+        <div class="mt-4">
+          <base-input
+            v-model="form.password_confirmation"
+            :label="$t('Confirm Password')"
+            type="password"
+            required
+            autocomplete="new-password"
+          />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+          <inertia-link
+            :href="route('login')"
+            class="underline text-sm text-gray-600 hover:text-gray-900"
+          >
+            {{ $t('Already registered?') }}
+          </inertia-link>
+
+          <base-button class="ml-4" :loading="form.processing">
+            {{ $t('Register') }}
+          </base-button>
+        </div>
+      </form>
+    </auth-card>
+  </div>
+</template>
+
+<script lang="ts">
+  import { useRoute } from '@admin/plugins/route'
+  import { useForm } from '@inertiajs/inertia-vue3'
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    setup() {
+      const form = useForm({
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+      })
+
+      const submit = () => {
+        form.post(useRoute('register'), {
+          onFinish: () => form.reset('password', 'password_confirmation'),
+        })
+      }
+
+      return { form, submit }
+    },
+  })
+</script>
