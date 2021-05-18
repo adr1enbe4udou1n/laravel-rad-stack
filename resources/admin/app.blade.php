87d7b5686e836e274b3a1c857ac61c1d394830fb
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-05-18 10:07:54 +0200

    format

--- resources/admin/app.blade.php
+++ resources/admin/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,7 +12,9 @@
     @translations
     @vite(admin/app.ts)
 </head>
+
 <body class="font-sans antialiased">
     @inertia
 </body>
+
 </html>
--- resources/front/app.blade.php
+++ resources/front/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,7 +11,9 @@
 
     @vite(front/app.ts)
 </head>
+
 <body class="antialiased">
     @livewireScripts
 </body>
+
 </html>
