07a68063440503fb050a7bd23500194dc2cc08d9
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-06-10 11:41:00 +0200

    trust all proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var null|array|string
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
