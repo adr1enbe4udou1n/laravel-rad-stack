9f820dea23d356beac0c62a1130f3e6169f7c3d1
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-06-15 09:40:28 +0200

    parallelism

--- tests/Feature/ViteManifestTest.php
+++ tests/Feature/ViteManifestTest.php
@@ -3,7 +3,21 @@
 use App\Support\LaravelViteManifest;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use function Pest\testDirectory;
+
+beforeEach(function () {
+    if (File::exists(public_path('dist'))) {
+        return;
+    }
+    File::makeDirectory(public_path('dist/front'), 0755, true);
+    File::copy(testDirectory('manifest.json'), public_path('dist/front/manifest.json'));
+});
+
+afterEach(function () {
+    File::deleteDirectory(public_path('dist'));
+});
 
 test('vite directive should return correct embed call', function () {
     $bladeSnippet = '@vite("front", "http://localhost:3100/app.ts")';
@@ -36,6 +50,7 @@ test('vite manifest return dev scripts', function () {
 
 test('vite manifest return production scripts', function () {
     Config::set('vite.dev_server', false);
+
     $url = Str::replace('/', '\/', asset('dist/front/assets'));
 
     $scripts = app(LaravelViteManifest::class)->embed(
