464d7bc7af173fd2335764b106ea51bb32ef129e
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-08-26 16:19:15 +0200

    ♻️ separe base from components

--- resources/admin/vite.config.ts
+++ resources/admin/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   plugins: [
     vue(),
     components({
-      dirs: ['components', 'layouts'],
+      dirs: ['base', 'components', 'layouts'],
       globalComponentsDeclaration: true,
     }),
     windicss({
